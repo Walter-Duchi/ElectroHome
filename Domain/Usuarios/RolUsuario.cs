9e75b5cb641b22d732323e8b5db94c3aab2c7025
Walter-Duchi <waltduchi@gmail.com>
2025-12-16 10:05:58 -0500

    creacion de Usuario y RolUsuario

---
+++ Domain/Usuarios/RolUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Usuarios
+{
+    public enum RolUsuario
+    {
+        Cliente,
+        Revisor,
+        Tecnico,
+        PersonalEntrega
+    }
+}
---
+++ Domain/Usuarios/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Usuarios
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Correo { get; private set; }
+        public RolUsuario Rol { get; private set; }
+
+        protected Usuario() { }
+
+        public Usuario(string Correo, RolUsuario Rol)
+        {
+            this.Id = 0;
+            this.Correo = Correo;
+            this.Rol = Rol;
+        }
+    }
+}
